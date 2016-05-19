e3de85811bfcc406d617d125ce8aefee9bd604f0
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-05-20 00:02:12 +0800

    Using integrate.IntegrationTest as the standard to test

--- qset_lww_test.go
+++ qset_lww_test.go
@@ -2,10 +2,9 @@ package qset
 
 import (
 	"testing"
-	"time"
 
 	"github.com/garyburd/redigo/redis"
-	"github.com/kavehmz/crdt"
+	"github.com/kavehmz/crdt/integrate"
 )
 
 func TestQSet_integration(t *testing.T) {
@@ -16,29 +15,5 @@ func TestQSet_integration(t *testing.T) {
 	add := setupSetNoInit(t, &ac, &asc, "TESTADD")
 	remove := setupSetNoInit(t, &rc, &rsc, "TESTREMOVE")
 
-	lww := lww.LWW{AddSet: add, RemoveSet: remove}
-	lww.Init()
-	e := "e1"
-	ts := time.Now()
-
-	if lww.Exists(e) {
-		t.Error("New LWW claims to containt an element")
-	}
-
-	lww.Add(e, ts)
-	if !lww.Exists(e) {
-		t.Error("Newly added element does not exists and it should")
-	}
-
-	ts = ts.Add(time.Second)
-	lww.Remove(e, ts)
-	if lww.Exists(e) {
-		t.Error("An element which was remove with a more recent timestmap must be removed and is not")
-	}
-
-	ts = ts.Add(time.Second)
-	lww.Add(e, ts)
-	if !lww.Exists(e) {
-		t.Error("An element which was remove and added again with a more recent timestamp does not exists")
-	}
+	integrate.IntegrationTest(add, remove, t)
 }
